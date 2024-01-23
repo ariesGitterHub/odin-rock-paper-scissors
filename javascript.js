e55e6b34dde1b6d3fc7de90557d3ef8d204a569f
ariesGitterHub <149708069+ariesGitterHub@users.noreply.github.com>
2024-01-22 21:24:53 -0500

    Many changes to HTML, CSS, and JS; code functions as intended

--- javascript.js
+++ javascript.js
@@ -1,154 +1,212 @@
 // Just naming the five elements to avoid using quotation marks throughout the code.
 
-const wood = "Wood";
-const fire = "Fire";
-const earth = "Earth";
-const metal = "Metal";
-const water = "Water";
+const wo = "Wood";
+const fi = "Fire";
+const ea = "Earth";
+const me = "Metal";
+const wa = "Water";
 
-// Various counter variables.
+const btnWo = document.querySelector("#btn-wo");
+const btnFi = document.querySelector("#btn-fi");
+const btnEa = document.querySelector("#btn-ea");
+const btnMe = document.querySelector("#btn-me");
+const btnWa = document.querySelector("#btn-wa");
 
-let gamesPlayed = 0;
-let myWins = 0;
-let myLosses = 0;
-let ties = 0;
-let compWins = 0;
-let compLosses = 0;
+const myPick = document.querySelector("#my-pick")
+const defaultYouChoose = "Please choose an element from the five above."
 
-// Generating random numbers for random element picks.
+const compPick = document.querySelector("#comp-pick");
+const defaultCompChoose = "Your opponent awaits your secret selection.";
 
-const myRandNum = Math.floor(Math.random() * 5 + 1);
-// console.log(myRandNum);
-const compRandNum = Math.floor(Math.random() * 5 + 1);
-// console.log(compRandNum);
+const winMsg = document.querySelector("#win-msg");
+const defaultPreGame = "The ancient elements are currently at rest.";
 
-const getMyElem = () => {
-  switch (myRandNum) {
-    case 1:
-      return wood;
-    case 2:
-      return fire;
-    case 3:
-      return earth;
-    case 4:
-      return metal;
-    default:
-      return water;
-  }
-};
+const curWinsText = document.querySelector("#cur-wins");
+const curLossesText = document.querySelector("#cur-losses");
 
-let myElem = getMyElem()
-// console.log(`You chose: ${getMyElem()}.`);
-console.log(`You chose: ${myElem}.`);
+let myElem;
+let compElem;
+let gameCount = 0;
+let curWins = 0;
+let curLosses = 0; // aka computer wins.
+let rando = 0;
 
-const getCompElem = () => {
-  switch (compRandNum) {
-    case 1:
-      return wood;
-    case 2:
-      return fire;
-    case 3:
-      return earth;
-    case 4:
-      return metal;
-    default:
-      return water;
-  }
-};
-let compElem = getCompElem()
-// console.log(`Your opponent chose: ${getCompElem()}.`);
-console.log(`Your opponent chose: ${compElem}.`);
-
-const outcomeMessage = () => {
-  let message = "";
-  if (
-    (myElem === fire && compElem === wood) ||
-    (myElem === wood && compElem === fire)
-  ) {
-    return (message = "Fire devours Wood");
-  } else if (
-    (myElem === metal && compElem === wood) ||
-    (myElem === wood && compElem === metal)
-  ) {
-    return (message = "Metal severs Wood");
-  } else if (
-    (myElem === earth && compElem === fire) ||
-    (myElem === fire && compElem === earth)
-  ) {
-    return (message = "Earth smothers Fire");
-  } else if (
-    (myElem === water && compElem === fire) ||
-    (myElem === fire && compElem === water)
-  ) {
-    return (message = "Water extinguishes Fire");
-  } else if (
-    (myElem === metal && compElem === earth) ||
-    (myElem === earth && compElem === metal)
-  ) {
-    return (message = "Metal hides within Earth");
-  } else if (
-    (myElem === wood && compElem === earth) ||
-    (myElem === earth && compElem === wood)
-  ) {
-    return (message = "Wood depletes Earth");
-  } else if (
-    (myElem === water && compElem === metal) ||
-    (myElem === metal && compElem === water)
-  ) {
-    return (message = "Water rusts Metal");
-  } else if (
-    (myElem === fire && compElem === metal) ||
-    (myElem === metal && compElem === fire)
-  ) {
-    return (message = "Fire melts Metal");
-  } else if (
-    (myElem === wood && compElem === water) ||
-    (myElem === water && compElem === wood)
-  ) {
-    return (message = "Wood drinks Water");
-  } else {
-    return (message = "Earth directs and contains Water");
-  }
-};
-
-const getWinner = () => {
-  if (
-    (myElem === fire && compElem === wood) ||
-    (myElem === metal && compElem === wood) ||
-    (myElem === earth && compElem === fire) ||
-    (myElem === water && compElem === fire) ||
-    (myElem === metal && compElem === earth) ||
-    (myElem === wood && compElem === earth) ||
-    (myElem === water && compElem === metal) ||
-    (myElem === fire && compElem === metal) ||
-    (myElem === wood && compElem === water) ||
-    (myElem === earth && compElem === water)
-  ) {
-    return `You win!!! ${outcomeMessage()}`;
-  } else if (myElem === compElem) {
-    return "Tie match. Replay until there is a winner.";
-  } else {
-    return `You lose. ${outcomeMessage()}`;
-  }
-};
-
-
-
-function playRound(myElem, compElem) {
-  if (myElem && compElem) {
-    console.log(myElem);
-    return gamesPlayed === 1;
-  }
+function compRandNum() {
+//   let randomNum = Math.floor(Math.random() * 5 + 1);
+return Math.floor(Math.random() * 5 + 1);
 }
 
-playRound()
+btnWo.addEventListener("click", getMyElem);
+btnFi.addEventListener("click", getMyElem);
+btnEa.addEventListener("click", getMyElem);
+btnMe.addEventListener("click", getMyElem);
+btnWa.addEventListener("click", getMyElem);
 
 
-console.log(getWinner());
-console.log(`Games played: ${gamesPlayed}`);
-console.log(`My wins: ${myWins}`);
-console.log(`My losses: ${myLosses}`);
-console.log(`Ties: ${ties}`);
-console.log(`Opponent wins: ${compWins}`);
-console.log(`Opponent losses: ${compLosses}`);
+function playRound() {
+  if (gameCount === 0) {
+    myPick.textContent = defaultYouChoose;
+    compPick.textContent = defaultCompChoose;
+    winMsg.textContent = defaultPreGame;
+    curWinsText.textContent = `${curWins}`;
+    curLossesText.textContent = `${curLosses}`;
+    }
+  }
 
+playRound();
+
+function getMyElem(e) {
+  if (e.target.id === "btn-wo" || e.target.id === "img-wo") {
+    myElem = wo;
+    rando = compRandNum();
+    myPick.textContent = `You chose the element of ${wo}.`;
+  } else if (e.target.id === "btn-fi" || e.target.id === "img-fi") {
+    myElem = fi;
+    rando = compRandNum();
+    myPick.textContent = `You chose the element of ${fi}.`;
+  } else if (e.target.id === "btn-ea" || e.target.id === "img-ea") {
+    myElem = ea;
+    rando = compRandNum();
+    myPick.textContent = `You chose the element of ${ea}.`;
+  } else if (e.target.id === "btn-me" || e.target.id === "img-me") {
+    myElem = me;
+    rando = compRandNum();
+    myPick.textContent = `You chose the element of ${me}.`;
+  } else if (e.target.id === "btn-wa" || e.target.id === "img-wa") {
+    myElem = wa;
+    rando = compRandNum();
+    myPick.textContent = `You chose the element of ${wa}.`;
+    } 
+
+  function getCompElem() {
+    if (myElem) {
+      if (rando === 1) {
+        compElem = wo;
+          compPick.textContent = `Your opponent selected ${wo}.`;
+      } else if (rando === 2) {
+        compElem = fi;
+        compPick.textContent = `Your opponent selected ${fi}.`;
+      } else if (rando === 3) {
+        compElem = ea;
+        compPick.textContent = `Your opponent selected ${ea}.`;
+      } else if (rando === 4) {
+        compElem = me;
+        compPick.textContent = `Your opponent selected ${me}.`;
+      } else if (rando === 5) {
+        compElem = wa;
+        compPick.textContent = `Your opponent selected ${wa}.`;
+      } else {
+        console.log("There is currently no player selection.");
+      }
+      return compElem;
+    }
+  }
+
+  getCompElem();
+
+  function outcomeMessage() {
+    let message;
+    if (
+      (myElem === fi && compElem === wo) ||
+      (myElem === wo && compElem === fi)
+    ) {
+      return (message = "Fire devours Wood");
+    } else if (
+      (myElem === me && compElem === wo) ||
+      (myElem === wo && compElem === me)
+    ) {
+      return (message = "Metal severs Wood");
+    } else if (
+      (myElem === ea && compElem === fi) ||
+      (myElem === fi && compElem === ea)
+    ) {
+      return (message = "Earth smothers Fire");
+    } else if (
+      (myElem === wa && compElem === fi) ||
+      (myElem === fi && compElem === wa)
+    ) {
+      return (message = "Water extinguishes Fire");
+    } else if (
+      (myElem === me && compElem === ea) ||
+      (myElem === ea && compElem === me)
+    ) {
+      return (message = "Metal hides within Earth");
+    } else if (
+      (myElem === wo && compElem === ea) ||
+      (myElem === ea && compElem === wo)
+    ) {
+      return (message = "Wood depletes Earth");
+    } else if (
+      (myElem === wa && compElem === me) ||
+      (myElem === me && compElem === wa)
+    ) {
+      return (message = "Water rusts Metal");
+    } else if (
+      (myElem === fi && compElem === me) ||
+      (myElem === me && compElem === fi)
+    ) {
+      return (message = "Fire melts Metal");
+    } else if (
+      (myElem === wo && compElem === wa) ||
+      (myElem === wa && compElem === wo)
+    ) {
+      return (message = "Wood drinks Water");
+    } else {
+      return (message = "Earth directs and contains Water");
+    }
+  }
+
+  outcomeMessage();
+
+  function getWinner() {
+    if (
+      (myElem === fi && compElem === wo) ||
+      (myElem === me && compElem === wo) ||
+      (myElem === ea && compElem === fi) ||
+      (myElem === wa && compElem === fi) ||
+      (myElem === me && compElem === ea) ||
+      (myElem === wo && compElem === ea) ||
+      (myElem === wa && compElem === me) ||
+      (myElem === fi && compElem === me) ||
+      (myElem === wo && compElem === wa) ||
+      (myElem === ea && compElem === wa)
+    ) {
+      winMsg.textContent = `${outcomeMessage()}, you win a point!`;
+      gameCount++;
+      curWins += 1;
+      curWinsText.textContent = `${curWins}`;
+    } else if (myElem === compElem) {
+      winMsg.textContent = `Tie!!! 
+      Replay until there is a winner.`;
+      gameCount++;
+    } else {
+      winMsg.textContent = `${outcomeMessage()}. Opponent gains a point.`;
+      gameCount ++;
+      curLosses+=1;
+      curLossesText.textContent = `${curLosses}`;
+    }
+  }
+
+  getWinner();
+
+  function matchWinnerOrLoser() {
+    if (curWins === 5) {
+      winMsg.textContent = `You win with 5 points! (${outcomeMessage()}.)`
+    } else if (curLosses === 5) {
+      winMsg.textContent = `Opponent has 5 points, you lose. (${outcomeMessage()}.)`;
+    } 
+  }
+
+  matchWinnerOrLoser();
+
+  function resetGame() {
+    if (curWins === 5 || curLosses === 5) {
+      setTimeout(function () {
+        alert("Clock OK to begin a new Wu Xing Roshambo match.");
+        location.reload();
+        return false;
+      }, 100);
+      }
+    }
+  resetGame()
+}
